177b4ceb129fa6c9b9e4b9dcc5142ff7f8ce6125
叶自青 <1634488412@qq.com>
2022-07-22 12:01:19 +0800

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.tsx
@@ -0,0 +1,29 @@
+/*
+ * @Description: 
+ * @Autor: yzq
+ * @Date: 2022-07-22 11:50:55
+ * @LastEditors: yzq
+ */
+import * as React from 'react'
+import { Link } from 'gatsby'
+import {container} from './layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Description: 
+ * @Autor: yzq
+ * @Date: 2022-07-21 18:00:04
+ * @LastEditors: yzq
+ */
+import * as React from "react"
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+    
+  )
+}
+
+export default About
